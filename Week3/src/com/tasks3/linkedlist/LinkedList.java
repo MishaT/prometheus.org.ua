d0f377103c41acdd7b032010b15efe41cb17fdb0
Misha Tolkachov <misha.tolkachov@gmail.com>
2017-03-11 02:15:11 +0200

    stub for the task 3

---
+++ Week3/src/com/tasks3/linkedlist/LinkedList.java
@@ -0,0 +1,34 @@
+package com.tasks3.linkedlist;
+
+
+public class LinkedList {
+	
+	public LinkedList() {
+		//PUT YOUR CODE HERE
+		//PUT YOUR CODE HERE
+	}
+
+	public void add(Integer data) {
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+	}
+
+	public Integer get(int index) {
+		return index;
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+	}
+
+	public boolean delete(int index) {
+		return false;
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+	}
+
+	public int size() {
+		return 0;
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+	}
+}   
+      
