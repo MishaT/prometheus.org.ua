fdc32cf32e50d6c6d67385d482452f863584f104
Misha Tolkachov <misha.tolkachov@gmail.com>
2017-04-16 22:04:38 +0300

    Fibonacci number

---
+++ Week3/src/com/tasks3/fibonacci/Fibonacci.java
@@ -0,0 +1,15 @@
+package com.tasks3.fibonacci;
+
+public class Fibonacci {
+    // Повертає число Фібоначчі за номером, нумерація почнеться з одиниці
+    // якщо число не можливо вирахувати поверніть -1
+    public long getNumber(int position) {
+        if (position == 1 || position == 2) {
+            return 1;
+        } else {
+            long n1 = getNumber(position - 1);
+            long n2 = getNumber(position - 2);
+            return n1 + n2;
+        }
+    }
+}
